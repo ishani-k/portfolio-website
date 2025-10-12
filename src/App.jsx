5bb1d446e76961c5f9fc2a629234a9856f6b08d5
ishani-k <ishanikundu.ik@gmail.com>
2025-10-12 23:17:04 +0530

    Setting up project

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import './App.css'
+import About from './components/About'
+import Contact from './components/Contact'
+import HeroSection from './components/HeroSection'
+import Navbar from './components/Navbar'
+import Projects from './components/Projects'
+import Skills from './components/Skills'
+
+function App() {
+
+  return (
+    <>
+    <div className="bg-gray-50 text-gray-800">
+      <Navbar/>
+      <HeroSection />
+      <About />
+      <Skills />
+      <Projects />
+      <Contact />
+    </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/HeroSection.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const HeroSection = () => {
+  return (
+    <div>
+      <h1 className="text-3xl md:text-5xl lg:text-6xl font-bold">
+        Hello, I'm Ishani 
+      </h1> 
+
+    </div>
+  )
+}
+
+export default HeroSection
