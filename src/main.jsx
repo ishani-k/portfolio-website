2ae15e50e751da226e2cff4a785d10c151e45742
ishani-k <ishanikundu.ik@gmail.com>
2025-10-26 12:34:53 +0530

    added spped insights

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { inject } from '@vercel/analytics'
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 inject()
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
-  </StrictMode>,
+    <SpeedInsights/>
+  </StrictMode>
 )
